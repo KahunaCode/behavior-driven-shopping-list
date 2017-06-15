ae87ea44d29dda829e42c2615e554a9f6e8b327b
Alexis <alexisko@hawaii.edu>
2017-06-14 21:11:53 -1000

    Next test created

--- test/shopping_list_test.js
+++ test/shopping_list_test.js
@@ -3,9 +3,17 @@ var chai = require('chai');
 var expect = chai.expect;
 var should = chai.should();
 
+var SLI = require('../js/shopping_list_item.js');
+
 describe('ShoppingListItem', function(){
 
+  var ShoppingListItem = SLI;
+
   it('should be a class', function() {
-    expect(ShoppingListItem).to.be.a('class');
+    expect(ShoppingListItem).to.be.a('function');
+  });
+
+  it('should have a property named "name"', function() {
+    expect(ShoppingListItem).to.have.property('name');
   });
 });
