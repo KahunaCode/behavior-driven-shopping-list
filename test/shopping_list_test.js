d88056f2b306bb2ff8250b73af0f4d546b49f654
James Moore <jameslmoore@gmail.com>
2017-06-14 20:52:32 -1000

    test for class

---
+++ test/shopping_list_test.js
@@ -0,0 +1,11 @@
+var chai = require('chai');
+
+var expect = chai.expect;
+var should = chai.should();
+
+describe('ShoppingListItem', function(){
+
+  it('should be a class', function() {
+    expect(ShoppingListItem).to.be.a('class');
+  });
+});
