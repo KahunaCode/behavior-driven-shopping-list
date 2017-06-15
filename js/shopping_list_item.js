a75e21926605d10d0e9bddeb694f2753c4b0a35e
Alexis <alexisko@hawaii.edu>
2017-06-14 22:11:40 -1000

    Added is_done property

--- js/shopping_list_item.js
+++ js/shopping_list_item.js
@@ -1,11 +1,10 @@
 /*jshint esversion: 6 */
 class ShoppingListItem {
-  constructor(name, description, is_done) {
+  constructor(name, description) {
     this.name = name;
     this.description = description;
-    this.is_done = is_done;
+    this.is_done = false;
   }
-
 
 }
 
