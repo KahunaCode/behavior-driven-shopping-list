fb550c4e9a0aedad08729e2041ec5feb484c6f36
Alexis <alexisko@hawaii.edu>
2017-06-15 21:37:37 -1000

    Started functionality

---
+++ js/app.js
@@ -0,0 +1,19 @@
+var button = document.getElementById('add_shopping_list_item_button');
+var content = document.getElementById('content');
+
+var shoppingList = new ShoppingList();
+var new_shopping_list_item;
+
+var title, description;
+
+button.addEventListener('click', function() {
+  add_to_shopping_list();
+});
+
+function add_to_shopping_list() {
+  title = document.getElementById('title').value;
+  description = document.getElementById('description').value;
+  console.log(title + " " + description);
+  new_shopping_list_item = new ShoppingListItem(title, description);
+  console.log(new_shopping_list_item);
+}
