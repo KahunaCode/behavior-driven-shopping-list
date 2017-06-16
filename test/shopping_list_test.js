1c7117df056907844706d31511a5c6866c2be44d
Alexis <alexisko@hawaii.edu>
2017-06-15 18:15:29 -1000

    Added class test

--- test/shopping_list_test.js
+++ test/shopping_list_test.js
@@ -4,7 +4,7 @@ var chai = require('chai');
 var expect = chai.expect;
 var should = chai.should();
 
-var SLI = require('../js/shopping_list_item.js');
+var SLI = require('../js/shopping_list.js');
 
 describe('ShoppingListItem', function(){
 
@@ -16,42 +16,7 @@ describe('ShoppingListItem', function(){
   });
 
   it('should be a class', function() {
-    expect(ShoppingListItem).to.be.a('function');
+    expect(milk.items).to.not.equal(undefined);
   });
 
-  it('should have a property named name', function() {
-    expect(milk.name).to.equal('milk');
-  });
-
-  it('should have property named description', function(){
-    expect(milk.description).to.equal('cows');
-  });
-
-  it('should have a property named is_done', function() {
-    expect(milk.is_done).to.equal(false);
-  });
-
-  it('has constructor method with two arguments name and description', function() {
-      expect(ShoppingListItem).to.have.lengthOf(2);
-      expect(milk.name).to.not.equal(undefined);
-      expect(milk.description).to.not.equal('description');
-  });
-
-  it('has a method named check', function() {
-    expect(milk.check).to.be.a('function');
-    milk.check();
-    expect(milk.is_done).to.equal(true);
-  });
-
-  it('has a method named uncheck', function() {
-    expect(milk.uncheck).to.be.a('function');
-    milk.uncheck();
-    expect(milk.is_done).to.equal(false);
-  });
-
-  it('has a method named render', function() {
-    expect(milk.render).to.be.a('function');
-    expect(milk.render()).to.be.a('string');
-    expect(milk.render()).to.equal(`<li class="completed_${milk.is_done}"><span>${milk.name}</span> <span>${milk.description}</span>`);
-  });
 });
