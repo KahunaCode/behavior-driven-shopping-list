52c60089728a60e9b1ac65155c43384fedde47f9
James Moore <jameslmoore@gmail.com>
2017-06-15 19:46:17 -1000

    try catch addItem

--- js/shopping_list.js
+++ js/shopping_list.js
@@ -3,6 +3,16 @@ class ShoppingList {
   constructor(items) {
     this.items = [];
   }
+
+  addItem(thing){
+    try {
+      this.items.push(thing);
+    }
+    catch  (e) {
+      console.log(e);
+    }
+  }
+
 }
 
 module.exports = ShoppingList;
